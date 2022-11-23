d35763c639d35c4895684a5d37c782bc322a32ce
Artem Peshkov <pewkoff@gmail.com>
2022-11-23 21:47:31 +0300

    Initial Commit

---
+++ DataStore.swift
@@ -0,0 +1,56 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Lepestok Jora on 23.11.2022.
+//
+
+import Foundation
+
+class DataStore {
+    static let shared = DataStore()
+    
+    let nameArey: [String] = ["Сергей",
+                            "Антон",
+                            "Денис",
+                            "Ирина",
+                            "Ольга",
+                            "Евгения",
+                            "Александр",
+                            "Анна",
+                            "Андрей",
+                            "Жора"]
+    let surArey: [String] = ["Сергеев",
+                           "Котов",
+                           "Карпова",
+                           "Романова",
+                           "Морозова",
+                           "Невский",
+                           "Коренина",
+                           "Богомолов",
+                           "Лепестков"]
+    
+    let mailArey: [String] = ["Сергеев@mail.ru",
+                              "Котов@mail.ru",
+                              "Карпова@mail.ru",
+                              "Романова@mail.ru",
+                              "Морозова@mail.ru",
+                              "Невский@mail.ru",
+                              "Коренина@mail.ru",
+                              "Богомолов@mail.ru",
+                              "Лепестков@mail.ru"]
+    
+    
+    let telArey: [String] = ["11-111",
+                           "22-222",
+                           "33-333",
+                           "44-444",
+                           "55-5555",
+                           "66-6666",
+                           "77-7777",
+                           "88-8888",
+                           "99-9999"]
+    
+    
+    
+}
---
+++ NameContact.swift
@@ -0,0 +1,47 @@
+//
+//  Name.swift
+//  ContactList
+//
+//  Created by Lepestok Jora on 23.11.2022.
+//
+import Foundation
+
+struct NameContact{
+    let id:UUID = UUID()
+    let name: String
+    let surName: String
+    let telefon: String
+    let mail: String
+    
+    var title: String {
+        "\(name) \(surName)"
+    }
+}
+
+extension NameContact: Hashable{
+    static func getContactList() -> [NameContact] {
+        var nameContactArrey: [NameContact] = []
+        // в цикле проходим 10 раз и каждый раз создаем уникального чувака которго помещаем в пустойц массив
+        
+        for _ in 0...9 {
+            let newChel =  NameContact (name: DataStore.shared.nameArey.randomElement()!,
+                                        surName: DataStore.shared.surArey.randomElement()!,
+                                        telefon: DataStore.shared.telArey.randomElement()!,
+                                        mail: DataStore.shared.mailArey.randomElement()!
+            )
+            nameContactArrey.append(newChel)
+            
+        }
+        return nameContactArrey
+    }
+        
+        
+    static func random() -> [NameContact] {
+        var result : [NameContact] = []
+        while result.count < NameContact.getContactList().count {
+            let randomIndex = Int(arc4random_uniform(UInt32(NameContact.getContactList().count)))
+            result.append(NameContact.getContactList()[randomIndex])
+        }
+        return result
+    }
+}
